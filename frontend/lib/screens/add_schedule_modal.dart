08fe70adb060c72e1eb61803daf942d101369979
yelimk <kyl7708@gmail.com>
2026-09-14 19:52:50 +0900

    style: 사용하지 않는 api_exception 임포트 제거

--- frontend/lib/screens/add_schedule_modal.dart
+++ frontend/lib/screens/add_schedule_modal.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../services/api_client.dart';
-import '../services/api_exception.dart';
 import '../services/calendar_service.dart';
 import '../theme/app_colors.dart';
 import '../theme/app_text_styles.dart';
