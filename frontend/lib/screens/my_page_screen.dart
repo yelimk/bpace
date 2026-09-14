193d1b41d7699ceb07cefabc41ff29f6cc9e232e
yelimk <kyl7708@gmail.com>
2026-09-14 21:58:14 +0900

    fix: 마이페이지 User 모델 import 추가

--- frontend/lib/screens/my_page_screen.dart
+++ frontend/lib/screens/my_page_screen.dart
@@ -5,6 +5,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../theme/app_colors.dart';
 import '../theme/app_text_styles.dart';
 import '../utils/responsive.dart';
+import '../models/user.dart';
 import '../services/api_client.dart';
 import '../services/auth_service.dart';
 import '../services/push_service.dart';
