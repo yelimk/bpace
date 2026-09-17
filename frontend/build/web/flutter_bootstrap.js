db1cb5ca62a46be16c77d3facd6e863960dab3d9
yelimk <kyl7708@gmail.com>
2026-09-17 21:26:27 +0900

    docs & build: README 수정 및 웹 빌드 업데이트

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1906992334" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "855031999" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
